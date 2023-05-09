3ad04a0e3c283dabc756055225545be55182508a
Crenstonw <b2016rubensa@gmail.com>
2023-05-09 19:40:46 +0200

    minor changes added

--- src/main/java/com/salesianostriana/dam/proyecto3rtrimestre/model/ParteVehiculo.java
+++ src/main/java/com/salesianostriana/dam/proyecto3rtrimestre/model/ParteVehiculo.java
@@ -7,10 +7,8 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
