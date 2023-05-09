752c02ca60f22a55f98e43db2ad6a7a0379d1108
Crenstonw <b2016rubensa@gmail.com>
2023-05-09 19:01:08 +0200

    arreglado algunos bugs, descubierto el doble

---
+++ src/main/java/com/salesianostriana/dam/proyecto3rtrimestre/repos/RepositorioTrabajador.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.proyecto3rtrimestre.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salesianostriana.dam.proyecto3rtrimestre.model.Trabajador;
+
+public interface RepositorioTrabajador extends JpaRepository<Trabajador, Long> {
+
+}
