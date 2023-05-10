5f01c3b326a2a5c3c3df605ee7d7f31ad43c0fcb
Alejandro Rubens <b2016rubensa@gmail.com>
2023-05-10 10:36:11 +0200

    minor changes added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
-insert into Cliente (cod_Cliente, dni, nombre, apellidos, telefono, email) values (1, "926383728N", "Pepe", "Fanegas Deleto", "959632748", "pepefan@gmail.com");
+insert into Cliente (cod_cliente, dni, nombre, apellidos, telefono, email) values (1, '926383728N', 'Pepe', 'Fanegas Deleto', '959632748', 'pepefan@gmail.com');
+insert into Cliente (cod_cliente, dni, nombre, apellidos, telefono, email) values (2, '534109283F', 'Antonio', 'Vicente Cortés', '738028374', 'veceantonio@gmail.com');
 
 ALTER SEQUENCE hibernate_sequence RESTART WITH 1000;
