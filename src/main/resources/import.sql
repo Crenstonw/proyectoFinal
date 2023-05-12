09221f317af13ef25707c4263eb886fa05bff103
Alejandro Rubens <b2016rubensa@gmail.com>
2023-05-12 13:25:05 +0200

    algun que otro dato añadido en la base de datos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 insert into Cliente (cod_cliente, dni, nombre, apellidos, telefono, email) values (7, '926383728N', 'Pepe', 'Fanegas Deleto', '959632748', 'pepefan@gmail.com');
 insert into Cliente (cod_cliente, dni, nombre, apellidos, telefono, email) values (4, '534109283F', 'Antonio', 'Vicente Cortés', '738028374', 'veceantonio@gmail.com');
 
---insert into Trabajador (id_Trabajador, fecha_Nacimiento, dni, nombre, apellidos, foto_trabajador, is_Admin) values (1, "03-02-1999", '819473842M', 'Trevor', 'Santo Triste', 'https://robohash.org/129837', true);
+insert into Trabajador (id_trabajador, fecha_nacimiento, dni, nombre, apellidos, foto_trabajador, is_admin) values (1, '1999-02-03', '819473842M', 'Trevor', 'Santo Triste', 'https://robohash.org/129837', false);
 
 ALTER SEQUENCE hibernate_sequence RESTART WITH 1000;
