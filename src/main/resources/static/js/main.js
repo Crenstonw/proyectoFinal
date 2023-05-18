f78d60a35d8822f897797bdb390ac07ca2b3526d
Crenstonw <b2016rubensa@gmail.com>
2023-05-18 16:56:49 +0200

    varios arreglos

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,13 +2,23 @@
 function alerting() {
 	return alert("me he cargado");
 }
-let nom = document.getElementById('nombre');
+let nom = document.getElementById('nombre').value;
 document.getElementById('fotoTrabajador').addEventListener('focus', function() {
 	if(this.value == '') {
 	  this.value = 'http://robohash.org/';
-	}
-
-	if(nom.value != '' && this.value == '' || this.value == 'http://robohash.org/') {
+	} else {
 		this.value = 'http://robohash.org/' + nom;
 	}
+	
+	/*if(nom.value != '' && this.value == '' || this.value == 'http://robohash.org/') {
+		this.value = 'http://robohash.org/' + nom;
+	}*/
   });
+  
+  let cosa = document.getElementById('tipoVehiculo').value;
+  
+  function cositas(cosa) {
+	  if(cosa == document.getElementsByName('option').value) {
+		  
+	  }
+  }
